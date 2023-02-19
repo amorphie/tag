fe183e3575b79f6d5288f3dad3fba0f2bc0c55fb
Sercan Yüksel <72672537+SercanYuksel1@users.noreply.github.com>
2023-02-19 18:05:24 +0300

    Migrate Project Added

---
+++ amorphie.tag.migrate/Migrate.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using amorphie.tag.data;
+
+namespace amorphie.tag.migrate
+{
+    public class Migrate : BackgroundService
+    {
+        private readonly ILogger<Migrate> _logger;
+        private readonly IHostApplicationLifetime _host;
+        private readonly TagDBContext _databaseContext;
+        public Migrate(ILogger<Migrate> logger,IHostApplicationLifetime host,TagDBContext databaseContext)
+        {
+            _logger = logger;
+            _host = host;
+            _databaseContext = databaseContext;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await _databaseContext.Database.MigrateAsync();
+            _host.StopApplication();
+        }
+    }
+}
