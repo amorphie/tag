a71dae4a3631241ed60fca085e1975d54fd155a2
yarasaa <mehmet.akbaba2011@gmail.com>
2023-07-03 13:47:38 +0300

    execute fix

--- amorphie.tag.execute/Program.cs
+++ amorphie.tag.execute/Program.cs
@@ -46,7 +46,21 @@ app.UseSwaggerUI();
 app.MapGet("/tag/{domainName}/{entityName}/{tagName}/execute", ExecuteTag)
 .WithOpenApi(operation =>
 {
-    operation.Summary = "Executes given tag with using query parameters.";
+    operation.Summary = "Executes given entity with using tag-in query- parameters.";
+    return operation;
+})
+
+
+.Produces(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status500InternalServerError)
+.Produces(StatusCodes.Status510NotExtended)
+.Produces(StatusCodes.Status400BadRequest)
+.Produces(StatusCodes.Status204NoContent);
+
+app.MapGet("/tag/{tagName}/execute", TagExecute)
+.WithOpenApi(operation =>
+{
+    operation.Summary = "Executes given tag with using query parameters and returns all entity fields.";
     return operation;
 })
 .Produces(StatusCodes.Status200OK)
@@ -68,17 +82,17 @@ app.MapGet("/tag/{tagName}/ugur", () => { })
 .Produces(StatusCodes.Status400BadRequest)
 .Produces(StatusCodes.Status204NoContent);
 
-app.MapGet("/domain/{domainName}/entity/{entityName}/Execute", ExecuteEntity)
-.WithOpenApi(operation =>
-{
-    operation.Summary = "Executes given entity with using tag-in query- parameters.";
-    return operation;
-})
-.Produces(StatusCodes.Status200OK)
-.Produces(StatusCodes.Status500InternalServerError)
-.Produces(StatusCodes.Status510NotExtended)
-.Produces(StatusCodes.Status400BadRequest)
-.Produces(StatusCodes.Status204NoContent);
+// app.MapGet("/domain/{domainName}/entity/{entityName}/Execute", ExecuteEntity)
+// .WithOpenApi(operation =>
+// {
+//     operation.Summary = "Executes given entity with using tag-in query- parameters.";
+//     return operation;
+// })
+// .Produces(StatusCodes.Status200OK)
+// .Produces(StatusCodes.Status500InternalServerError)
+// .Produces(StatusCodes.Status510NotExtended)
+// .Produces(StatusCodes.Status400BadRequest)
+// .Produces(StatusCodes.Status204NoContent);
 
 try
 {
@@ -230,79 +244,79 @@ async Task<IResult> ExecuteTag(
 
 
 
-async Task<IResult> ExecuteEntity(
-    [FromRoute(Name = "domainName")] string domainName,
-    [FromRoute(Name = "entityName")] string entityName,
-    [FromQuery(Name = "tag")] string[] tags,
-    [FromQuery(Name = "reference")] string reference,
-    HttpRequest request,
-    HttpContext httpContext
-)
-{
-    if (tags == null || tags.Length == 0)
-    {
-        return Results.BadRequest("At least one tag must be supplied.");
-    }
+// async Task<IResult> ExecuteEntity(
+//     [FromRoute(Name = "domainName")] string domainName,
+//     [FromRoute(Name = "entityName")] string entityName,
+//     [FromQuery(Name = "tag")] string[] tags,
+//     [FromQuery(Name = "reference")] string reference,
+//     HttpRequest request,
+//     HttpContext httpContext
+// )
+// {
+//     if (tags == null || tags.Length == 0)
+//     {
+//         return Results.BadRequest("At least one tag must be supplied.");
+//     }
 
-    var queryReference = reference?.Split('&').ToList();
-    var queryTags = request.Query["tag"].ToList();
-    try
-    {
-        var entity = await client.InvokeMethodAsync<GetEntityResponse>(
-            HttpMethod.Get,
-            $"{amorphie_tag}",
-            $"entityData/getEntity/{domainName}/{entityName}"
-        );
+//     var queryReference = reference?.Split('&').ToList();
+//     var queryTags = request.Query["tag"].ToList();
+//     try
+//     {
+//         var entity = await client.InvokeMethodAsync<GetEntityResponse>(
+//             HttpMethod.Get,
+//             $"{amorphie_tag}",
+//             $"entityData/getEntity/{domainName}/{entityName}"
+//         );
 
-        var returnValue = new Dictionary<string, dynamic>();
+//         var returnValue = new Dictionary<string, dynamic>();
 
-        foreach (var field in entity.Data)
-        {
-            var sourceTags = field.Sources.OrderBy(f => f.Order).ToArray();
+//         foreach (var field in entity.Data)
+//         {
+//             var sourceTags = field.Sources.OrderBy(f => f.Order).ToArray();
 
-            foreach (var targetTag in sourceTags)
-            {
-                if (queryTags.Contains(targetTag.Tag))
-                {
-                    var data = await client.InvokeMethodAsync<dynamic>(
-                                        HttpMethod.Get,
-                                        "amorphie-tag-execute",
-                                        $"tag/{domainName}/{entityName}/{targetTag.Tag}/execute?reference={queryReference?.FirstOrDefault()}");
-                    JToken dataAsJson = JToken.Parse(data.ToString());
+//             foreach (var targetTag in sourceTags)
+//             {
+//                 if (queryTags.Contains(targetTag.Tag))
+//                 {
+//                     var data = await client.InvokeMethodAsync<dynamic>(
+//                                         HttpMethod.Get,
+//                                         "amorphie-tag-execute",
+//                                         $"tag/{domainName}/{entityName}/{targetTag.Tag}/execute?reference={queryReference?.FirstOrDefault()}");
+//                     JToken dataAsJson = JToken.Parse(data.ToString());
 
-                    if (dataAsJson.SelectToken(targetTag.Path) != null)
-                    {
-                        returnValue.Add(field.Field, dataAsJson.SelectToken(targetTag.Path)!.Value<string>()!);
-                    }
+//                     if (dataAsJson.SelectToken(targetTag.Path) != null)
+//                     {
+//                         returnValue.Add(field.Field, dataAsJson.SelectToken(targetTag.Path)!.Value<string>()!);
+//                     }
 
-                    break;
-                }
+//                     break;
+//                 }
 
-            }
-        }
+//             }
+//         }
 
 
-        return Results.Ok(returnValue);
-    }
-    catch (Dapr.Client.InvocationException ex)
-    {
-        if (ex.Response.StatusCode == HttpStatusCode.NotFound)
-        {
-            return Results.NotFound("Entity is not found.");
-        }
+//         return Results.Ok(returnValue);
+//     }
+//     catch (Dapr.Client.InvocationException ex)
+//     {
+//         if (ex.Response.StatusCode == HttpStatusCode.NotFound)
+//         {
+//             return Results.NotFound("Entity is not found.");
+//         }
 
-        if (ex.Response.StatusCode == HttpStatusCode.InternalServerError)
-        {
-            return Results.Problem("Entity query service is unavailable", null, 510);
-        }
+//         if (ex.Response.StatusCode == HttpStatusCode.InternalServerError)
+//         {
+//             return Results.Problem("Entity query service is unavailable", null, 510);
+//         }
 
-        return Results.Problem($"Entity query service error: {ex.Response.StatusCode}", null, 510);
-    }
-    catch (Exception ex)
-    {
-        return Results.Problem($"Unhandled Entity query service error: {ex.Message}", null, 510);
-    }
-}
+//         return Results.Problem($"Entity query service error: {ex.Response.StatusCode}", null, 510);
+//     }
+//     catch (Exception ex)
+//     {
+//         return Results.Problem($"Unhandled Entity query service error: {ex.Message}", null, 510);
+//     }
+// }
 
 
 async Task<IResult> TemplateExecuter(
@@ -619,7 +633,77 @@ async Task<IResult> TemplateExecuteTag(
     {
         return Results.Problem($"Unhandled Entity query service error: {ex.Message}", null, 510);
     }
+}
 
 
+    async Task<IResult> TagExecute(
+    [FromRoute(Name = "tagName")] string tagName,
+    HttpRequest request,
+    HttpContext httpContext
+    )
+{
+    app.Logger.LogInformation("ExecuteTag is calling");
+
+    DtoTag tag;
+
+    try
+    {
+        tag = await client.InvokeMethodAsync<DtoTag>(HttpMethod.Get, $"{amorphie_tag}", $"Tag/getTag/{tagName}");
+    }
+    catch (Dapr.Client.InvocationException ex)
+    {
+        if (ex.Response.StatusCode == HttpStatusCode.NotFound)
+            return Results.NotFound("Tag is not found.");
+
+        if (ex.Response.StatusCode == HttpStatusCode.InternalServerError)
+            return Results.Problem("Tag query service is unavailable", null, 510);
+
+        return Results.Problem($"Tag query service error : {ex.Response.StatusCode}", null, 510);
+    }
+    catch (Exception ex)
+    {
+
+        return Results.Problem($"Unhandled Tag query service error : {ex.Message}", null, 510);
+    }
+
+    if (string.IsNullOrEmpty(tag.Url))
+    {
+        return Results.BadRequest("This tag does not have URL");
+    }
+
+    var parameters = tag.Url.Split(new Char[] { '/', '?', '&', '=' }, StringSplitOptions.RemoveEmptyEntries).Where(x => x.StartsWith('@')).ToList();
+    var urlToConsume = tag.Url;
+    foreach (var p in parameters)
+    {
+        if (!request.Query.ContainsKey(p.TrimStart('@')))
+            return Results.BadRequest($"Required Url parameter(s) is not supplied as query parameters. Required parameters : {string.Join(",", parameters)}");
+        // Düzeltildi
+        urlToConsume = urlToConsume.Replace(p, request!.QueryString.Value!.TrimStart('?').Split('&').FirstOrDefault(x => x.StartsWith(p.TrimStart('@')))!.Split('=').LastOrDefault() ?? string.Empty);
+        //urlToConsume = urlToConsume.Replace(p, request.Query.FirstOrDefault(x => x.Value != p).ToString());
+    }
+
+
+    var cachedResponse = await client.GetStateAsync<dynamic>(STATE_STORE, urlToConsume);
+
+    if (cachedResponse is not null)
+    {
+        httpContext.Response.Headers.Add("X-Cache", "Hit");
+        return Results.Ok(cachedResponse);
+    }
+    else
+    {
+        HttpClient httpClient = new();
+        var response = await httpClient.GetFromJsonAsync<dynamic>(urlToConsume);
+
+        var metadata = new Dictionary<string, string> { { "ttlInSeconds", $"{tag.Ttl}" } };
+        await client.SaveStateAsync(STATE_STORE, urlToConsume, response, metadata: metadata);
+
+        httpContext.Response.Headers.Add("X-Cache", "Miss");
+
+
+        app.Logger.LogInformation($"ExecuteTag is responded with {response}");
+        return Results.Ok(response);
+    }
 };
+
 
